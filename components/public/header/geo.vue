d6f60157b561ca4add83fbe60e0ecd09dc1eb0a6
zhanghuan <734776328@qq.com>
2019-07-21 08:53:29 +0800

    init my object

---
+++ components/public/header/geo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location" />广州市
+    <nuxt-link class="changeCity" to="changeCity">
+      切换城市
+    </nuxt-link>
+    [茂名 东莞 湛江]
+  </div>
+</template>
+  
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+  
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-row class="layout-default">
+    <el-col>
+      <my-header />
+    </el-col>
+    <el-col>
+      <nuxt />
+    </el-col>
+    <el-col>
+      <my-footer />123
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import MyHeader from '../components/public/header/index.vue'
+import MyFooter from '../components/public/footer/index.vue'
+export default {
+  components: {
+    MyHeader
+  }
+}
+</script>
+<style lang="scss" scope>
+
+</style>
