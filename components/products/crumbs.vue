51caecfdecb975074a0ebd256969e1493d598ebf
zhanghuan <734776328@qq.com>
2019-09-03 21:35:38 +0800

    完成产品详情页

---
+++ components/products/crumbs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="m-crumbs">
+    <el-breadcrumb >
+      <el-breakcrumb-item 
+      :to="{path: '/'}"
+      >
+        {{$store.state.geo.position.city.replace('市','')}}美团
+      </el-breakcrumb-item>
+      <el-breadcrumb-item>
+        <a href="/">
+          {{ $store.state.geo.position.city.replace('市','')}}
+          {{decodeURIComponent(keyword)}}
+        </a>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+  
+<script>
+export default {
+  props: {
+    keyword: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped="scss">
+  
+</style>
+  
---
+++ pages/product.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <crumbs/>
+  </div>
+</template>
+
+<script>
+import crumbs from '../components/products/crumbs.vue'
+export default {
+  components: {
+    crumbs
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
